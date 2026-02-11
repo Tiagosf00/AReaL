ae21297f6d707c1394c4c5162bbdfa322d4faf8e
Tiago Fernandes <tiagotsf2000@gmail.com>
2026-02-11 18:51:20 +0800

    Update setup.sh for cm

--- setup.sh
+++ setup.sh
@@ -3,14 +3,35 @@ set -e  # Exit immediately if any command fails
 
 echo "🚀 Starting environment setup..."
 
+apt update
+apt install -y \
+  make build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev \
+  libxmlsec1-dev libffi-dev liblzma-dev curl git
+
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+pyenv install 3.12.3
+pyenv global 3.12.3
+
 # 1. Install python libraries
-pip install uv huggingface_hub datasets pytest torch
-pip install -e .
+pip install huggingface_hub datasets pytest tensorboard
 
 # 2. Install uv 
 echo "📦 Installing uv..."
-pip install uv
+python -m pip install -U uv
 uv sync --extra cuda
+
+uv pip install -e . --no-deps
+
+# uv run python3 areal/tools/validate_installation.py
+
 
 # 3. Run download scripts
 echo "📥 Downloading datasets..."
@@ -21,6 +42,8 @@ python3 models/download_model.py
 
 echo "✅ Setup complete!"
 
+# 4. Run tests
 
-# Container image:
-# ghcr.io/inclusionai/areal-runtime:v0.5.3
+uv run python -m areal.launcher.local examples/math/gsm8k_rl.py --config examples/math/gsm8k_grpo_lora.yaml
+
+uv run python -m areal.launcher.local examples/math/gsm8k_rl.py --config examples/math/gsm8k_grpo.yaml
